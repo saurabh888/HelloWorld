df11f46626e11ad827f1c1254d15eb9d853b40aa
Chamedia <chamedia@amazon.com>
2016-11-20 15:40:06 +0530

    Added ImprovedHelloWorld class

---
+++ HelloWorld/src/chamedia/ImprovedHelloWorld.java
@@ -0,0 +1,14 @@
+package chamedia;
+
+public class ImprovedHelloWorld {
+	
+	public static void main(String[] args){
+
+		if(args.length > 0){
+			System.out.println("Hello " + args[0]);
+		}
+		else{
+			System.out.println("Hello World");
+		}
+	}
+}
